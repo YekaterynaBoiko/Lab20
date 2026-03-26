a516c1458a08e5f996e6b2789db2e81d80cc9a6d
Yekateryna Boiko <yekateryna.boyko@gmail.com>
2026-03-26 23:10:38 +0200

    add update in master branch

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             // Блок привітання
-            Console.WriteLine("Ласкаво просимо до Калькулятора!");
+            Console.WriteLine("Супер Калькулятор v2.0 (Оновлено)!");
+            Console.WriteLine("----------------------------------");
 
             // Блок обчислень
             Console.WriteLine("Тут будуть результати:");
